bb3cd68a6da08c18d9a0c8212c4e2cc09270d98f
Maui <maui.baba1@gmail.com>
2024-03-04 19:36:02 -0500

    Added mac address discovery to master

--- esp32-master/src/main.cpp
+++ esp32-master/src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// MAC discovery tool, uncomment if needed
+void setup(void)
+{
+  // If random characters are displayed, make sure to add baud_rate = 9600 to platformio.ini
+  Serial.begin(9600);
+  Serial.println("-----------------");
+  uint8_t macBT[6];
+  esp_read_mac(macBT, ESP_MAC_BT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  uint8_t macBT[6];
+  esp_read_mac(macBT, ESP_MAC_BT);
+  Serial.printf("%02X:%02X:%02X:%02X:%02X:%02X\r\n", macBT[0], macBT[1], macBT[2], macBT[3], macBT[4], macBT[5]);
+  delay(10000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// Master code
